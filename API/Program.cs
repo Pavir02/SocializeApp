d63da0f5a3f38b170f27eb8de8b82a4872fccf73
Pavithra R <pavi.r02@gmail.com>
2022-12-21 08:04:58 +0530

    Updating to dotnet 7 and use minimum hosting model

--- API/Program.cs
+++ API/Program.cs
@@ -1,26 +1,51 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using API.Data;
+using API.Extensions;
+using API.Middleware;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
-namespace API
-{
-    public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Services container
+    builder.Services.AddApplicationServices(builder.Configuration);
+    builder.Services.AddIdentityServices(builder.Configuration);
+    builder.Services.AddControllers();
+    builder.Services.AddCors();
+    // builder.Services.AddSwaggerGen(c =>
+    // {
+    //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+    // });
+
+// Middleware
+    var app = builder.Build();
+    app.UseMiddleware<ExceptionMiddleware>();
+
+    // if (env.IsDevelopment())
+    // {
+    //     //app.UseDeveloperExceptionPage();
+    //     app.UseSwagger();
+    //     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+    // }
+ 
+    app.UseHttpsRedirection(); //http requests will be redirected to https
+    app.UseRouting(); //To setup routing mechanism
+    app.UseCors(x=> x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.MapControllers();
+           
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    try
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        var context = services.GetRequiredService<DataContext>();
+        await context.Database.MigrateAsync();
+        await Seed.SeedUsers(context);
     }
-}
+    catch(Exception ex)
+    {
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+    }
+
+    app.Run();
